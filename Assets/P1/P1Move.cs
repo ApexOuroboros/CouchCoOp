e3489103911f3d90a5102c31a1e501a486fff5f8
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2023-02-03 18:24:59 +0000

    can rotate indiv - cant move indiv

--- Assets/P1/P1Move.cs
+++ Assets/P1/P1Move.cs
@@ -9,6 +9,8 @@ public class P1Move : MonoBehaviour
 {
     CharacterController p1Con;
 
+    //Rigidbody p1R;
+
     public float speed = 6.0f;
     public float jSpeed = 8.0f;
     public float grav = 20.0f;
@@ -18,6 +20,9 @@ public class P1Move : MonoBehaviour
     void Start()
     {
         p1Con = GetComponent<CharacterController> ();   
+
+        //p1R = GetComponent<Rigidbody> ();
+
     }
 
     // Update is called once per frame
@@ -26,8 +31,21 @@ public class P1Move : MonoBehaviour
         
         if(p1Con.isGrounded)
         {
-            mDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            mDir *= speed;
+            //mDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            //mDir *= speed;
+
+
+            if (Input.GetKey(KeyCode.A))
+                transform.Rotate(0, -1, 0);
+
+            if (Input.GetKey(KeyCode.D))
+                transform.Rotate(0, 1, 0);
+
+            if (Input.GetKey(KeyCode.W))
+                transform.position +=  Vector3.forward * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.S))
+                transform.position += Vector3.back * Time.deltaTime;
 
             if (Input.GetButton("Jump"))
             {

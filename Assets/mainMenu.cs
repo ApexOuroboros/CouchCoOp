d4f99b53d441f42f9a2198987fd1b431922f17b3
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2023-02-05 22:07:03 +0000

    main menu proper

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void PlayGame2p()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
+
 }
